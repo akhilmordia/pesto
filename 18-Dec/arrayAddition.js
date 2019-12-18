c094a5cda82f5c16906005e59542b80b5b41b6fb
Akhil <akhil@chtrbox.com>
2019-12-18 19:29:02 +0530

    "arrayAddition" function along-with test cases

---
+++ 18-Dec/arrayAddition.js
@@ -0,0 +1,33 @@
+/**
+ * Created by akhil on 18/12/19.
+ */
+
+//todo: fixing a silly mistake in my code. Pushing it for review.
+function arrayAddition(...arrays)
+{
+    let sumArray = [];
+    arrays.forEach(array =>
+    {
+        if (!Array.isArray(array) || array.length === 0)
+        {
+            throw "Parameter is not a valid array!";
+        }
+
+        array.forEach((value, index) =>
+        {
+            if (!Number.isInteger(value))
+            {
+                throw "value is not integer!";
+            }
+
+            if (!sumArray.includes(index))
+                sumArray[index] = 0;
+
+            sumArray[index] += value;
+        })
+    });
+
+    return sumArray;
+}
+
+module.exports = arrayAddition;
